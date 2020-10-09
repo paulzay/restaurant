d617078ee095d8db44473d474461880d701c066b
paulzay <paulotieno2@gmail.com>
2020-10-09 22:15:16 +0300

    fix about page

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,10 @@
 const about = () => {
   const footer = document.createElement('footer');
   footer.innerHTML = `
-        <div class="tabs_content" data-tab="4">
-            <h2>contact Us</h2>
+        <div class="contact" data-tab="4">
+            <h2>Hello</h2>
+            <p>What can we help you with?</p>
+            <p>Call us on 555-000-333</p>
         </div>
   `;
 
