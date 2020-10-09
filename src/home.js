15656495c988500b3cdbe65a8a6a9020006c783a
paulzay <paulotieno2@gmail.com>
2020-10-09 19:37:05 +0300

    styling

--- src/home.js
+++ src/home.js
@@ -1,12 +1,20 @@
 const home = () => {
-  const footer = document.createElement('footer');
-  footer.innerHTML = `
+  const content = document.createElement('div');
+  content.innerHTML = `
         <div class="tabs_content" data-tab="1">
-            <h2>home</h2>
+            <div class="welcometext">
+              <div class='words'>
+              <h1>ZAY'S KITCHEN</h1>
+              <p>NEW NORTHWEST AUSTIN LOCATION AT 183 & ANDERSON MILL!</p>
+              </div>
+              <div class="noods">
+                <img src="http://michiramen.com/media/hero-image-2.png"/>
+              </div>
+            </div>
         </div>
   `;
 
-  return footer;
+  return content;
 };
 
 export default home;
